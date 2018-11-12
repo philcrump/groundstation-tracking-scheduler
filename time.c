cbfcc7cda14c2a97619ddaf64522ea2c105c2310
Phil Crump <phil@philcrump.co.uk>
2018-11-12 12:23:34 +0000

    time.c: Fix timestamp_s() bug.

--- time.c
+++ time.c
@@ -5,11 +5,11 @@ uint64_t timestamp_s(void)
 {
   struct timespec spec;
   clock_gettime(CLOCK_REALTIME, &spec);
-  return (uint64_t) spec.tv_sec + (((uint64_t) spec.tv_nsec) / 1000);
+  return (uint64_t) spec.tv_sec + (((uint64_t) spec.tv_nsec) / (1000 * 1000 * 1000));
 }
 uint64_t timestamp_ms(void)
 {
   struct timespec spec;
   clock_gettime(CLOCK_REALTIME, &spec);
-  return ((uint64_t) spec.tv_sec) * 1000 + (((uint64_t) spec.tv_nsec) / 1000000);
+  return ((uint64_t) spec.tv_sec) * 1000 + (((uint64_t) spec.tv_nsec) / (1000 * 1000));
 }
